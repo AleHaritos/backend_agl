65439b558ac2fc2a8d68f5f991dc75902b67d9bc
AleHaritos <aleharitos@gmail.com>
2022-01-16 23:10:18 -0300

    Teste knexfile para deploy10

--- migrations/20220109050737_pedido_usuario.js
+++
@@ -1,9 +0,0 @@
-exports.up = function(knex) {
-  return knex.schema.alterTable('pedidos', table => {
-    table.integer('idUsuario').defaultTo(null)
-  })
-};
-
-exports.down = function(knex) {
-  
-};
