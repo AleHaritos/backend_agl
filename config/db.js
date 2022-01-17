86a3a035d5b1f32464881ad9ae3bc8ce0c91274f
AleHaritos <aleharitos@gmail.com>
2022-01-16 22:50:01 -0300

    Teste knexfile para deploy4

--- config/db.js
+++ config/db.js
@@ -3,5 +3,6 @@ const db_env = process.env.DB_ENV || 'development'
 const config = require('../knexfile')[db_env]
 const knex = require('knex')(config)
 
-knex.migrate.latest([config])
+await knex.migrate.latest([config])
+await knex.seed.run()
 module.exports = knex
