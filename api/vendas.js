55b5569bb3469c12d85fd791a6bd277f5310db7a
AleHaritos <aleharitos@gmail.com>
2022-02-18 16:53:23 -0300

    ajuste no metodo getVendasById

--- api/vendas.js
+++ api/vendas.js
@@ -131,7 +131,7 @@ const getVendasByIdPedido = (req, res) => {
 
     if(id){
     app.db('vendas')
-        .select('vendas.tamanho', 'vendas.quantidade', 'produtos.nome', 'pedidos.data')
+        .select('id.produtos', 'vendas.tamanho', 'vendas.quantidade', 'produtos.nome', 'pedidos.data')
         .innerJoin('produtos', 'vendas.idProduto', '=', 'produtos.id')
         .innerJoin('pedidos', 'vendas.idPedido', '=', 'pedidos.id')
         .where({ idPedido: id })
